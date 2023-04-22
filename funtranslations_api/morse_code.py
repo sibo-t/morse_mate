3aa101e4a8a20c9c01872c23dcb370483604ddd2
sibo-t <stmsabala@gmail.com>
2023-04-22 17:47:11 +0200

    old

--- funtranslations_api/morse_code.py
+++ funtranslations_api/morse_code.py
@@ -5,5 +5,5 @@ _base_url = "https://api.funtranslations.com/translate/"
 def eng_to_morse(text):
     data = dict(text=text)
     print(data)
-    translated = requests.post(_base_url+"morse.json",json=data).json()
+    translated = requests.post(_base_url+"morse.json").json()
     return translated["contents"]["translated"]
