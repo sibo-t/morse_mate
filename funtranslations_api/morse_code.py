536dafac11bedef6f8784da5d23c5b610f8b5379
Mbali Mabila <mbali.abila@derivco.co.za>
2023-04-22 17:43:10 +0200

    translate_morse_to_text

--- funtranslations_api/morse_code.py
+++ funtranslations_api/morse_code.py
@@ -2,8 +2,14 @@ import requests
 
 _base_url = "https://api.funtranslations.com/translate/"
 
-def eng_to_morse(text):
+def translate_text_to_morse(text):
     data = dict(text=text)
-    print(data)
     translated = requests.post(_base_url+"morse.json",json=data).json()
     return translated["contents"]["translated"]
+
+def translate_morse_to_text(morse_code):
+    data = {"text" : morse_code}
+    response = requests.post(_base_url+"morse.json",data=data)
+    if response.status_code == 200:
+        return response.json()["contents"]["translated"]
+    return None
--- test_morse_code.py
+++ test_morse_code.py
@@ -4,8 +4,13 @@ from funtranslations_api.morse_code import *
 class TestMorseCode(unittest.TestCase):
 
     def test_text_to_morsecode(self):
-        translate = eng_to_morse("Sibonelo says hi")
+        translate = translate_text_to_morse("Sibonelo says hi")
         self.assertEqual(translate, "... .. -... --- -. . .-.. ---     ... .- -.-- ...     .... .. ")
+
+    def test_morse_code_to_text(self):
+        translate = translate_morse_to_text("... .. -... --- -. . .-.. ---     ... .- -.-- ...     .... .. ")
+        self.assertEqual(translate,"Sibonelo says hi")
+
 
 
 if __name__ == '__main__':
